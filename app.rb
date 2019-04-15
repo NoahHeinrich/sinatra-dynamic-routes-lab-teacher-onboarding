5e6af07eb630687494f33878197e82c4dd5acfb0
Noa Heinrich <noah.heinrich@flatironschool.com>
2019-04-15 18:55:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,24 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do 
+    @reverse = params["name"].reverse
+    erb :reversename
+  end
+  
+  get '/square/:number' do 
+    @square_num = params["number"].to_i * params["number"].to_i
+    erb :square
+  end
+  
+  get '/say/:number/:phrase' do 
+    @number = params["number"].to_i
+    @phrase = params["phrase"]
+    erb :say
+  end
 
+  get '/say/:word1/:word2/:word3/:word4/:word5' do 
+    @long_string = params.values.join(" ") + "."
+    erb :say
+  end
 end
